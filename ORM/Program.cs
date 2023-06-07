84abbc507b97aaf334c5950782b529cafd222c4b
Seth Bowman <brandonsethbowman@gmail.com>
2023-06-07 08:25:58 -0500

    Practice lecture material

--- ORM/Program.cs
+++ ORM/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using ORM;
+using System.Threading.Channels;
+using System.Transactions;
+//^^^^MUST HAVE USING DIRECTIVES^^^^
+
+var config = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json")
+				.Build();
+string connString = config.GetConnectionString("DefaultConnection");
+IDbConnection conn = new MySqlConnection(connString);
+
+var repo = new DapperDepartmentRepository(conn);
+
+//Console.Write("Type a new Department name: ");
+
+//var newDepartment = Console.ReadLine();
+
+//repo.InsertDepartment(newDepartment);
+
+var departments  = repo.GetAllDepartments();
+
+foreach (var department in departments)
+{
+    Console.WriteLine(department.Name);
+}
